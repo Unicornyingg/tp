723bb142dce12b93ea12cfae5121c5db543725fc
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-03-13 13:00:17 +0800

    Complete the setup of main coupled with the classes to support it

---
+++ src/main/java/seedu/duke/Command.java
@@ -0,0 +1,6 @@
+package seedu.duke;
+
+public abstract class Command {
+    public abstract void execute(RecordList list);
+    public boolean isExit() {return false;}
+}
---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,15 @@
+package seedu.duke;
+
+public class Parser {
+    public static Command parse (String userInput) {
+        String[] split = userInput.split(" ");
+        String keyword = split[0].toLowerCase();
+        Record r;
+
+        switch(keyword) {
+        case "bye":
+            return new ExitCommand();
+        }
+        return null;
+    }
+}
